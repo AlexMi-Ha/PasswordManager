138a4911587fe13a748a74ea25a29dd287e0ee1d
Alex Hagl <alex@hagl.de>
2021-04-07 16:55:00 +0200

    Add project files.

---
+++ PasswordManager/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+namespace PasswordManager {
+
+    /// <summary>
+    /// Base class for all ViewModels
+    /// This Automatically handles all Property Changed Updates
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call to fire a PropertyChanged event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name) {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ PasswordManager/ViewModel/Base/RelayCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PasswordManager {
+    /// <summary>
+    /// Relay Command to run an action
+    /// </summary>
+    class RelayCommand : ICommand {
+
+        #region Public Events
+
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        #endregion
+
+        #region Private Attributes
+        /// <summary>
+        /// The action to run
+        /// </summary>
+        private Action action;
+        #endregion
+
+        #region Constructor
+        public RelayCommand(Action action) {
+            this.action = action;
+        }
+        #endregion
+
+        #region Command Methods
+        /// <summary>
+        /// RelayCommands can always execute
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the action
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter) {
+            action();
+        }
+        #endregion
+    }
+}
